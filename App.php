c4e966795bd7092afd133fc88ff64ecdafc23eb5
Leandro <veandro@outlook.com>
2017-02-13 20:49:41 -0800

    Update App.php

--- App.php
+++ App.php
@@ -1,11 +1,20 @@
-<?php 
-
+<?php
 namespace Lyra\Crux;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class App {
 
   public static function run() {
-    echo "working";
+    $request = Request::createFromGlobals();
+
+    $input = $request->get('name', 'World');
+
+    $response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+    $response->send();
   }
 }
 
